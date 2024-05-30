1a0da741dfc957b1be768f6645c111c21ebe5149
Moqian Chen <moqian.chen@student.kuleuven.be>
2024-05-30 19:41:02 +0200

    update layout and functions

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# packages need to be specified in the requirements.txt
 import pandas as pd
 from datetime import datetime
 import googlemaps
@@ -14,11 +10,10 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 
-# packages do not need to be specified in the requirements.txt
 import re
 import math
 import json
-
+import base64
 
 class AEDPoint:
     def __init__(self, id, x, y, address, indication, schedule, taken):
@@ -34,7 +29,7 @@ class AEDPoint:
         if self.x < -90 or self.x > 90 or self.y < -90 or self.y > 90:
             return math.inf
         return geodesic((self.x, self.y), loc).km
-
+    
 class MapAEDS:
     def __init__(self):
         self.points = {}
@@ -66,25 +61,21 @@ class MapAEDS:
         distances.sort(key = lambda v : v[1])
         top_24 = distances[:24]
         return dict([(key, (self.points[key][0].x, self.points[key][0].y)) for key, _ in top_24])
-
+    
 def fallback_position(ip_addr):
     ipinfo_url = "https://ipinfo.io/{}/json?token=89232058aa0651".format(ip_addr)
     response = json.loads(requests.post(ipinfo_url).content)['loc'].split(',')
     return (float(response[0]), float(response[1]))
 
-
 def is_in_schedule(schedule, datetime_obj):
-    # Get the day of the week from the datetime object
     day_of_week = datetime_obj.strftime("%A")
 
-    # Check if the day is in the schedule
     if day_of_week not in schedule:
         return True 
 
     if schedule[day_of_week] == 0:
         return False
 
-    # Get the intervals for the day from the schedule
     intervals = schedule[day_of_week]
     intervals = intervals.strip('[]').split(', ')
 
@@ -95,11 +86,9 @@ def is_in_schedule(schedule, datetime_obj):
         interval_lst = [element.strip() for element in splits]
         intervals_fix.append(interval_lst)
 
-
     for start, end in intervals_fix:
         pattern = r'^\d{2}'
     
-        # Use the search function to find if the pattern matches the string
         match_start = re.search(pattern, start)
         match_end = re.search(pattern, end)
         if bool(match_start) == True:
@@ -111,6 +100,15 @@ def is_in_schedule(schedule, datetime_obj):
                 new_num = int(end[:2]) - 12
                 end = str(new_num) + end[2:]
     
+        if start == '0 PM':
+            start = '12 PM'
+        if start == '0 AM':
+            start = '12 AM'
+        if end == '0 PM':
+            end = '12 PM'
+        if end == '0 AM':
+            end = '12 AM'
+        
         try:
             start_time = datetime.strptime(start, '%I %p').time()
         except ValueError:
@@ -135,38 +133,39 @@ def is_in_schedule(schedule, datetime_obj):
                 else:
                     raise ValueError(f"Time data '{end}' does not match format '%I:%M %p'")      
 
-        # Check if the datetime falls within this interval
         if start_time <= datetime_obj.time() <= end_time:
             return True
 
-    # If none of the intervals match, return False
     return False
-
-
 
 gmaps = googlemaps.Client(key='AIzaSyCHT_UZ49tsasFakgDUVha05snFVsUbq-M')
 
 def find_quickest_destinations(start_location, destinations):
-    # Calculate travel times to each destination
-    travel_times = {}
+    travel_times_driving = {}
+    travel_times_walking = {}
     for id, destination in destinations.items():
-        directions = gmaps.directions(start_location, destination, mode="driving")
-        if directions:
-            travel_time = directions[0]['legs'][0]['duration']['text']
-            travel_times[id] = travel_time
-        else:
-            print(f"No route found for destination: {destination}")
+        directions_driving = gmaps.directions(start_location, destination, mode="driving")
+        directions_walking = gmaps.directions(start_location, destination, mode="walking")
+        if directions_driving:
+            travel_time_driving = directions_driving[0]['legs'][0]['duration']['value']
+            travel_times_driving[id] = travel_time_driving
+        if directions_walking:
+            travel_time_walking = directions_walking[0]['legs'][0]['duration']['value']
+            travel_times_walking[id] = travel_time_walking
 
-    # # Find the destination with the shortest travel time
-    return min(travel_times, key=travel_times.get)
+    quickest_driving = min(travel_times_driving, key=travel_times_driving.get)
+    quickest_walking = min(travel_times_walking, key=travel_times_walking.get)
+
+    return quickest_driving, quickest_walking
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# Create a Dash application
 server = Flask(__name__)
 app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
 
-# user_location = user_loc
+test_png = 'AED_Bel.png'
+test_base64 = base64.b64encode(open(test_png, 'rb').read()).decode('ascii')
+
 maps = None
 
 def initialize_database():
@@ -175,16 +174,13 @@ def initialize_database():
     weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"]
     start_index = 15227
 
-    # Select the last 1000 rows starting from the defined index until the end and swap latitude and longitude columns within that subset
     last_1000_rows = all_aeds.iloc[start_index:]
     last_1000_rows = last_1000_rows.copy()
     last_1000_rows[['latitude', 'longitude']] = last_1000_rows[['longitude', 'latitude']]
 
-    # Update the last 1000 rows in the original DataFrame
     all_aeds.iloc[start_index:] = last_1000_rows
     all_aeds = all_aeds.dropna(subset=['longitude'])
 
-    # Reset index
     all_aeds = all_aeds.reset_index(drop=True)
 
     for column in weekdays:
@@ -207,7 +203,6 @@ def initialize_database():
             maps.update_availability(point, 1, time = current_time)
         i += 1
 
-
 @server.route('/update_location', methods=['POST'])
 def update_location():
     global user_location
@@ -215,63 +210,73 @@ def update_location():
     user_location = (data['latitude'], data['longitude'])
     return jsonify(success=True)
 
-# Function to generate the map as a folium object
-def generate_map(start_location, destination, destination_driving, destination_walking, display_driving, display_walking):
-    m = folium.Map(location=start_location, zoom_start=18)  # Increased zoom_start for closer view
+def generate_map(start_location, destination_driving, destination_walking, directions_driving, directions_walking):
+    m = folium.Map(location=start_location, zoom_start=18)
     
-    # Add marker for the user's current location
     folium.Marker(
         location=start_location,
         popup="Your Location",
         icon=folium.Icon(color="green")
     ).add_to(m)
-    
-    # Add marker for the AED destination
+
     folium.Marker(
-        location=destination,
-        popup="AED Location",
+        location=destination_driving,
+        popup="Nearest AED by Drive",
         icon=folium.Icon(color="red")
     ).add_to(m)
+
+    folium.Marker(
+        location=destination_walking,
+        popup="Nearest AED by Walk",
+        icon=folium.Icon(color="blue")
+    ).add_to(m)
     
-    if display_driving:
-        decoded_driving = polyline.decode(destination_driving)
+    if directions_driving:
+        decoded_driving = polyline.decode(directions_driving)
         folium.PolyLine(decoded_driving, color="red", weight=2.5, opacity=1, tooltip="Driving Route").add_to(m)
         
-    if display_walking:
-        decoded_walking = polyline.decode(destination_walking)
+    if directions_walking:
+        decoded_walking = polyline.decode(directions_walking)
         folium.PolyLine(decoded_walking, color="blue", weight=2.5, opacity=1, tooltip="Walking Route").add_to(m)
 
     return m
 
-# Update map based on latest location
-def update_map(user_location, quickest_destination, display_driving, display_walking):
+def update_map(user_location, quickest_driving, quickest_walking, selected_mode):
     global maps
-    directions_driving = gmaps.directions(user_location, (maps.points[quickest_destination][0].x, maps.points[quickest_destination][0].y), mode="driving")
-    directions_walking = gmaps.directions(user_location, (maps.points[quickest_destination][0].x, maps.points[quickest_destination][0].y), mode="walking")
-    
-    destination_driving = directions_driving[0]['overview_polyline']['points']
-    destination_walking = directions_walking[0]['overview_polyline']['points']
-    
-    destination = (maps.points[quickest_destination][0].x, maps.points[quickest_destination][0].y)
-    map_object = generate_map(user_location, destination, destination_driving, destination_walking, display_driving, display_walking)
-    
-    return map_object._repr_html_()
+    driving_location = (maps.points[quickest_driving][0].x, maps.points[quickest_driving][0].y)
+    walking_location = (maps.points[quickest_walking][0].x, maps.points[quickest_walking][0].y)
 
+    directions_driving = gmaps.directions(user_location, driving_location, mode="driving")
+    directions_walking = gmaps.directions(user_location, walking_location, mode="walking")
 
-# Define the layout of the app
+    directions_driving_polyline = directions_driving[0]['overview_polyline']['points']
+    directions_walking_polyline = directions_walking[0]['overview_polyline']['points']
+
+    if selected_mode == 'driving':
+        map_object = generate_map(user_location, driving_location, walking_location, directions_driving_polyline, None)
+    elif selected_mode == 'walking':
+        map_object = generate_map(user_location, driving_location, walking_location, None, directions_walking_polyline)
+    else:
+        map_object = generate_map(user_location, driving_location, walking_location, directions_driving_polyline, directions_walking_polyline)
+
+    return map_object._repr_html_(), directions_driving, directions_walking
+
 app.layout = html.Div([
-    html.H1("AED Locator", style={'text-align': 'center'}),
-    html.Img(src="AED_Bel.png")],
-    dcc.Geolocation(id='user_loc'),
-    
     html.Div([
-        dcc.Checklist(
+    html.Img(src='data:image/png;base64,{}'.format(test_base64), width=100, style={'display': 'inline-block'}),
+    html.H1("AED Locator", style={'text-align': 'center', 'display': 'inline-block'}),
+    dcc.Geolocation(id='user_loc'), ], 
+    ),
+
+    html.Div([
+        html.Label("Choose a mode of travel (You may only choose 1):"),
+        dcc.RadioItems(
             id='mode-selection',
             options=[
                 {'label': 'Driving', 'value': 'driving'},
                 {'label': 'Walking', 'value': 'walking'}
             ],
-            value=['driving', 'walking'],
+            value=None,
             labelStyle={'display': 'inline-block'}
         )
     ]),
@@ -282,7 +287,6 @@ app.layout = html.Div([
     ),
     html.Div(
         children=[
-            
             html.Div(
                 id='route-legend',
                 children=[
@@ -316,7 +320,11 @@ app.layout = html.Div([
                     ], style={'margin-bottom': '5px'}),
                     html.Div([
                         html.Img(src='https://maps.google.com/mapfiles/ms/icons/red-dot.png', style={'height': '20px', 'margin-right': '10px'}),
-                        "Location of AED"
+                        "Nearest AED by Drive"
+                    ]),
+                    html.Div([
+                        html.Img(src='https://maps.google.com/mapfiles/ms/icons/blue-dot.png', style={'height': '20px', 'margin-right': '10px'}),
+                        "Nearest AED by Walk"
                     ])
                 ],
                 style={
@@ -352,10 +360,23 @@ app.layout = html.Div([
     dcc.Store(id='user_state'),
     dcc.Interval(
         id='interval-component',
-        interval=10*1000,  # in milliseconds
+        interval=10*1000,
         n_intervals=0
     ),
     html.Div(id='status', style={'padding': '20px'}),
+    html.Div(id='directions-container', children=[
+        html.H2("Directions to AED", style={'margin-bottom': '10px'}),
+        html.Div(id='directions', style={
+            'border': '1px solid grey',
+            'border-radius': '3px',
+            'padding': '10px',
+            'background-color': 'white',
+            'font-family': 'Arial, sans-serif',
+            'font-size': '14px',
+            'max-height': '400px',  # Increased height for better visibility
+            'overflow-y': 'auto'  # Enable scrolling if content exceeds the container height
+        })
+    ], style={'padding': '20px', 'width': '90%', 'margin': '0 auto'}),  # Increased width for better visibility
     html.Div([
         html.Div([
             html.H2("How to Operate an AED", style={'margin-bottom': '10px'}),
@@ -378,58 +399,66 @@ app.layout = html.Div([
     ], style={'display': 'flex', 'flex-direction': 'row', 'align-items': 'flex-start'})
 ])
 
-# Callback to update the map and address
 @app.callback(
     [Output('map', 'srcDoc'),
      Output('address', 'children'),
-     Output('user_state', 'data')],
+     Output('user_state', 'data'),
+     Output('directions', 'children')],
     [Input('interval-component', 'n_intervals'),
      Input('mode-selection', 'value'),
      Input('user_loc', 'position'),
      Input('user_state','data')]
 )
-def update_map_interval(n, selected_modes, user_loc, user_state):
-    quickest_destination = None
+def update_map_interval(n, selected_mode, user_loc, user_state):
+    quickest_driving, quickest_walking = None, None
     if(user_loc == None):
-        # Try to get from IP address
         user_loc = fallback_position(request.remote_addr)
     else:
         user_loc = (user_loc['lat'], user_loc['lon'])
     previous_distance = None
     if(user_state == None):
-        # It means the webapp is just loaded. Check the location first
-        quickest_destination = find_quickest_destinations(user_loc, maps.points_within_radius(user_loc, 10.8))
+        quickest_driving, quickest_walking = find_quickest_destinations(user_loc, maps.points_within_radius(user_loc, 10.8))
         previous_distance = user_loc
     else:
-        quickest_destination = user_state['quickest_destination']
+        quickest_driving = user_state['quickest_driving']
+        quickest_walking = user_state['quickest_walking']
         previous_distance = user_state['previous_distance']
         if geodesic(user_loc, previous_distance).km > 5:
-            quickest_destination = find_quickest_destinations(user_loc, maps.points_within_radius(user_loc, 10.8))
+            quickest_driving, quickest_walking = find_quickest_destinations(user_loc, maps.points_within_radius(user_loc, 10.8))
             previous_distance = user_loc
-        
 
-    display_driving = 'driving' in selected_modes
-    display_walking = 'walking' in selected_modes
-    map_html = update_map(user_loc, quickest_destination, display_driving, display_walking)
-    address = maps.points[quickest_destination][0].address
-    return map_html, f"AED Location: {address}", { "quickest_destination" : quickest_destination, "previous_distance": previous_distance }
+    map_html, directions_driving, directions_walking = update_map(user_loc, quickest_driving, quickest_walking, selected_mode)
+    
+    if selected_mode == 'driving':
+        address = maps.points[quickest_driving][0].address
+        directions = directions_driving[0]['legs'][0]['steps']
+    elif selected_mode == 'walking':
+        address = maps.points[quickest_walking][0].address
+        directions = directions_walking[0]['legs'][0]['steps']
+    else:
+        address = maps.points[quickest_driving][0].address if quickest_driving == quickest_walking else f"Nearest AED by Drive: {maps.points[quickest_driving][0].address}\nNearest AED by Walk: {maps.points[quickest_walking][0].address}"
+        directions = []
 
-# Callback to update availability after 30 minutes
+    directions_text = []
+    if directions:
+        for step in directions:
+            instructions = re.sub('<[^<]+?>', '', step['html_instructions'])  # Remove HTML tags
+            directions_text.append(html.Li(instructions))
+
+    return map_html, f"AED Location: {address}", { "quickest_driving" : quickest_driving, "quickest_walking": quickest_walking, "previous_distance": previous_distance }, html.Ul(directions_text)
+
 @app.callback(Output('status', 'children'), [Input('interval-component', 'n_intervals'), Input('user_state','data')])
 def update_availability(n, user_state):
     if(user_state == None):
         return ""
-    quickest_destination = user_state['quickest_destination']
-    if n > 0 and n % 180 == 0:  # Every 30 minutes
-        maps.update_availability(quickest_destination, 2, sit='return')
+    quickest_driving = user_state['quickest_driving']
+    quickest_walking = user_state['quickest_walking']
+    if n > 0 and n % 180 == 0:
+        maps.update_availability(quickest_driving, 2, sit='return')
+        maps.update_availability(quickest_walking, 2, sit='return')
         return "AED has been returned and is now available again."
     return ""
 
-# Run the app
 initialize_database()
 if __name__ == '__main__':
-    app.run(jupyter_mode="external", port=8051, host="0.0.0.0")
-
-
-
-
+    app.run(jupyter_mode="external", port=8051)
